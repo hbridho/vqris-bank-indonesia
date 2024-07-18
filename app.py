ea977bee66ea4108330ba742b347d18e85368aa0
Ridho Botutihe <101564299+ridhobotutihe@users.noreply.github.com>
2024-07-18 20:59:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,5 @@ def get_cue_sound():
     return send_from_directory('static/voice', 'start_rec.wav')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
